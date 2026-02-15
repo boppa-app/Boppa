43a3029c263ff6860d493cab698443f319c5c1be
antonbezr <antonbezr@gmail.com>
2026-02-15 15:00:22 -0800

    init swift proj

---
+++ iOS/BopBrowser/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BopBrowser
+//
+//  Created by Bezruchkin, Anton on 2/15/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ iOS/BopBrowserTests/BopBrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  BopBrowserTests.swift
+//  BopBrowserTests
+//
+//  Created by Bezruchkin, Anton on 2/15/26.
+//
+
+import Testing
+@testable import BopBrowser
+
+struct BopBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
