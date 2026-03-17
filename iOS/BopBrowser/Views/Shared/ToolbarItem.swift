552f2ca1388a4911cfa0249a78014554bd46a99b
antonbezr <antonbezr@gmail.com>
2026-03-17 16:27:10 -0700

    autoformat

--- iOS/BopBrowser/Views/Shared/ToolbarItem.swift
+++ iOS/BopBrowser/Views/Shared/ToolbarItem.swift
@@ -4,13 +4,13 @@ extension ToolbarItem {
     @ToolbarContentBuilder
     func sharedBackgroundVisibilityIfAvailable(_ visibility: Visibility) -> some ToolbarContent {
         #if compiler(>=6.2)
-            if #available(iOS 26.0, *) {
-                sharedBackgroundVisibility(visibility)
-            } else {
-                self
-            }
-        #else
+        if #available(iOS 26.0, *) {
+            sharedBackgroundVisibility(visibility)
+        } else {
             self
+        }
+        #else
+        self
         #endif
     }
 }
