27fdc8856ad4ce752b7f93277310069d079a86aa
antonbezr <antonbezr@gmail.com>
2026-06-02 10:07:04 -0700

    fix: "Sort tracks" button text formatting

--- iOS/Boppa/Views/Tracklist/TracklistActionSheet.swift
+++ iOS/Boppa/Views/Tracklist/TracklistActionSheet.swift
@@ -142,7 +142,7 @@ struct TracklistActionSheet: View {
                     .font(.system(size: 16))
                     .foregroundColor(self.sortMode == .defaultOrder ? .white : .purp)
                     .frame(width: 24)
-                Text("Sort Tracks")
+                Text("Sort tracks")
                     .font(.body)
                     .foregroundColor(.white)
                     .padding([.trailing], -12)
@@ -162,7 +162,7 @@ struct TracklistActionSheet: View {
         .listRowBackground(Color(.systemGray6))
         .listRowInsets(EdgeInsets(top: 14, leading: 20, bottom: 14, trailing: 20))
         .listRowSeparator(.hidden)
-        .accessibilityLabel(self.sortMode == .defaultOrder ? "Sort Tracks" : "Sort Tracks: \(self.sortMode.label)")
+        .accessibilityLabel(self.sortMode == .defaultOrder ? "Sort tracks" : "Sort tracks: \(self.sortMode.label)")
         .accessibilityHint("Choose how tracks are sorted")
     }
 
