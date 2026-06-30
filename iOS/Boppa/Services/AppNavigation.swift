309d690ba68e7649af3b146f4669df75c6a2aa44
Anton Bezruchkin <antonbezr@gmail.com>
2026-06-30 10:28:42 -0700

    refactor: move navigation notification names and postTracklistNavigation to AppNavigation.swift

---
+++ iOS/Boppa/Services/AppNavigation.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+extension Notification.Name {
+    static let navigateToArtistInSearch = Notification.Name("navigateToArtistInSearch")
+    static let navigateToTracklistInSearch = Notification.Name("navigateToTracklistInSearch")
+    static let navigateToTracklistInLibrary = Notification.Name("navigateToTracklistInLibrary")
+}
+
+func postTracklistNavigation(_ tracklist: Tracklist) {
+    let isSavedToLibrary = TracklistStorageManager.shared.findStoredTracklist(
+        mediaId: tracklist.mediaId,
+        mediaSourceId: tracklist.mediaSourceId
+    )?.isSavedToLibrary == true
+    NotificationCenter.default.post(
+        name: isSavedToLibrary ? .navigateToTracklistInLibrary : .navigateToTracklistInSearch,
+        object: tracklist
+    )
+}
--- iOS/Boppa/Views/Search/SearchView.swift
+++ iOS/Boppa/Views/Search/SearchView.swift
@@ -1,22 +1,5 @@
 import SwiftUI
 
-extension Notification.Name {
-    static let navigateToArtistInSearch = Notification.Name("navigateToArtistInSearch")
-    static let navigateToTracklistInSearch = Notification.Name("navigateToTracklistInSearch")
-    static let navigateToTracklistInLibrary = Notification.Name("navigateToTracklistInLibrary")
-}
-
-func postTracklistNavigation(_ tracklist: Tracklist) {
-    let isSavedToLibrary = TracklistStorageManager.shared.findStoredTracklist(
-        mediaId: tracklist.mediaId,
-        mediaSourceId: tracklist.mediaSourceId
-    )?.isSavedToLibrary == true
-    NotificationCenter.default.post(
-        name: isSavedToLibrary ? .navigateToTracklistInLibrary : .navigateToTracklistInSearch,
-        object: tracklist
-    )
-}
-
 // TODO: Swiping up in search view should refresh results
 
 struct SearchView: View {
