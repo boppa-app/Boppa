14c9604159f0cc798a258cdf2194200e2c561b46
Anton Bezruchkin <antonbezr@gmail.com>
2026-09-16 14:35:44 -0700

    fix [iOS]: don't focus search bar when tab tap pops nav stack to root

--- iOS/Boppa/Views/ContentView.swift
+++ iOS/Boppa/Views/ContentView.swift
@@ -249,9 +249,11 @@ struct ContentView: View {
                                 withAnimation(.easeInOut(duration: 0.35)) {
                                     switch tab {
                                     case 0:
-                                        if !self
-                                            .searchIsAtRoot { self.searchNavigationReset.fire() }
-                                        self.searchFocusId += 1
+                                        if self.searchIsAtRoot {
+                                            self.searchFocusId += 1
+                                        } else {
+                                            self.searchNavigationReset.fire()
+                                        }
                                     case 1:
                                         if !self
                                             .libraryIsAtRoot { self.libraryNavigationReset.fire() }
